438184ca3d20b31cb4a8c4352c2269231430755e
Mig Salazar <mig.salazars@gmail.com>
2017-05-10 04:12:26 -0500

    Adding examples mod01

---
+++ mods/mod01/examples/MvcDemo/MvcDemo1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        
+        public ActionResult Welcome(string name, int times = 0)
+        {
+            ViewBag.Name = name;
+            ViewBag.Times = times;
+
+            return View();
+        }
+
+        [HttpPost]
+        public string Test(int id = 0) {
+            if (id == 1) {
+                return "uno";
+            }
+
+            return "cero";
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        
+
+    }
+}
---
+++ mods/mod01/examples/UsersDemo/UsersDemo/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace UsersDemo.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public decimal Weight { get; set; }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext() : base("UserDbContext") { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
